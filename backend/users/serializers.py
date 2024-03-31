c5dec7a876b016c6e70e6ad4f17ec6f818868882
EL IDRISSI Mohamed <eromex02@gmail.com>
2024-03-31 18:47:00 +0000

    custom jwt with user info

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from users.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -7,3 +9,14 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True}
         }
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """Customizes JWT default Serializer to add more information about user"""
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        token['email'] = user.email
+        token['is_staff'] = user.is_staff
+
+        return token
