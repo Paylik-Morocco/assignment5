b48473066a76264e1c4ab7a653757cdf202a3e1e
EL IDRISSI Mohamed <eromex02@gmail.com>
2024-03-31 18:46:20 +0000

    custom jwt with user info

--- backend/ticket_management/urls.py
+++ backend/ticket_management/urls.py
@@ -18,19 +18,20 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
-from users.views import get_profile, signup
+from users.views import get_profile, signup, CustomTokenObtainPairView
 from tickets.views import (all_tickets, get_ticket, create_ticket, update_ticket, create_ticket_reply)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/signup/', signup, name='token_obtain_pair'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', view=get_profile),
     path('api/tickets/', view=all_tickets),
     path('api/tickets/<int:id>/', view=get_ticket),
     path('api/tickets/create/', view=create_ticket),
-    path('api/tickets/<int:id>/update', view=update_ticket),
-    path('api/tickets/<int:id>/reply', view=create_ticket_reply),
+    path('api/tickets/<int:id>/update/', view=update_ticket),
+    path('api/tickets/<int:id>/reply/', view=create_ticket_reply),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
