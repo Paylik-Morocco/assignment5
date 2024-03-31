f09d5999fc935c5dad2fb0afac42e715833c05af
EL IDRISSI Mohamed <eromex02@gmail.com>
2024-03-31 18:46:49 +0000

    remove created_by serializer from ticketSerializer

--- backend/tickets/serializers.py
+++ backend/tickets/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from tickets.models import (Ticket, TicketReply)
 from users.serializers import UserSerializer
 class TicketSerializer(serializers.ModelSerializer):
-    created_by = UserSerializer(many=False)
     class Meta:
         model = Ticket
         fields = ['id', 'title', 'description', 'status', 'created_at', 'updated_at', 'created_by']
