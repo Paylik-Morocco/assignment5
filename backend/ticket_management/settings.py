b3d834b054497c901cb379b27a542bde287bfba9
EL IDRISSI Mohamed <eromex02@gmail.com>
2024-04-02 01:06:14 +0000

    use postgres

--- backend/ticket_management/settings.py
+++ backend/ticket_management/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,11 +79,18 @@ WSGI_APPLICATION = 'ticket_management.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+
+        'USER': os.environ.get('POSTGRES_USER'),
+
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+
+        'HOST': os.environ.get('POSTGRES_HOST'),
+
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 
@@ -142,6 +149,8 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
 ]
 
 APPEND_SLASH=True
