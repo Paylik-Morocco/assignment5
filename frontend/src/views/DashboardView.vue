4b813244cffdfb7b92c9dc5e174ad2682bec5e71
EL IDRISSI Mohamed <eromex02@gmail.com>
2024-04-02 18:27:42 +0000

    fetch tickets on create

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -41,38 +41,44 @@ const auth = useAuthStore();
 const ticketsStore = useTicketsStore();
 // ticketsStore.fetchTickets();
 
+// create ticket
 const submitTicket = async () => {
     const { status, data, response } = await axios.post('tickets/create/', {
         title: newTicket.value.title,
         description: newTicket.value.description,
     })
-    if(status === 201){
+    if (status === 201) {
         console.log(data);
         isShowModal.value = false;
-    }else{
+        fetchFilteredTickets()
+    } else {
         newTickerErrors.value = response.data;
     }
 }
-watch([searchFilter, datesFilter, statusFilter], (oldValue, newValue) => {
+
+function fetchFilteredTickets(){
     let status = statusFilter.value == 'all' ? null : statusFilter.value;
     let search = searchFilter.value;
     const now = moment();
     let start_datetime = null;
     let end_datetime = null;
-    
-    let params = {}
-    if(datesFilter.value == 'week'){
+
+    // if dates filter is 'week'. get all tickets from CURRENT week
+    if (datesFilter.value == 'week') {
         start_datetime = now.clone().startOf('week');
         end_datetime = now.clone().endOf('week');
     }
-    else if (datesFilter.value == 'month'){
+    // if dates filter is 'month'. get all tickets from CURRENT month
+    else if (datesFilter.value == 'month') {
         start_datetime = now.clone().startOf('month');
         end_datetime = now.clone().endOf('month');
     }
-    else if (datesFilter.value == 'today'){
+    // if dates filter is 'today'. get all tickets from today
+    else if (datesFilter.value == 'today') {
         start_datetime = now.clone().startOf('day');
         end_datetime = now.clone().endOf('day');
     }
+    //else get all tickets (implicit)
 
     ticketsStore.fetchTickets({
         status,
@@ -80,7 +86,10 @@ watch([searchFilter, datesFilter, statusFilter], (oldValue, newValue) => {
         end_datetime,
         search
     })
-}, {immediate: true})
+}
+watch([searchFilter, datesFilter, statusFilter], (oldValue, newValue) => {
+    fetchFilteredTickets()
+}, { immediate: true })
 
 ticketsStore.fetchTicketsOverview();
 </script>
@@ -91,27 +100,28 @@ ticketsStore.fetchTicketsOverview();
             <div
                 class="shadow-sm bg-amber-200 px-6 py-2 lg:py-6 rounded-xl flex flex-row lg:flex-row items-center justify-between">
                 <span class="block font-semibold text-md">Total Tickets</span>
-                <span class="block font-semibold lg:font-bold text-xl ml-4">{{ticketsStore.ticketsCount.all}}</span>
+                <span class="block font-semibold lg:font-bold text-xl ml-4">{{ ticketsStore.ticketsCount.all }}</span>
             </div>
             <div
                 class="shadow-sm bg-blue-400 px-6 py-2 lg:py-6 rounded-xl flex flex-row lg:flex-row items-center justify-between">
                 <span class="block font-semibold text-md">Open Tickets</span>
-                <span class="block font-semibold lg:font-bold text-xl ml-4">{{ticketsStore.ticketsCount.open}}</span>
+                <span class="block font-semibold lg:font-bold text-xl ml-4">{{ ticketsStore.ticketsCount.open }}</span>
             </div>
             <div
                 class="shadow-sm bg-slate-300 px-6 py-2 lg:py-6 rounded-xl flex flex-row lg:flex-row items-center justify-between">
                 <span class="block font-semibold text-md">Closed Tickets</span>
-                <span class="block font-semibold lg:font-bold text-xl ml-4">{{ticketsStore.ticketsCount.closed}}</span>
+                <span class="block font-semibold lg:font-bold text-xl ml-4">{{ ticketsStore.ticketsCount.closed }}</span>
             </div>
             <div
                 class="shadow-sm bg-emerald-400 px-6 py-2 lg:py-6 rounded-xl flex flex-row lg:flex-row items-center justify-between">
                 <span class="block font-semibold text-md">Resolved Tickets</span>
-                <span class="block font-semibold lg:font-bold text-xl ml-4">{{ticketsStore.ticketsCount.resolved}}</span>
+                <span
+                    class="block font-semibold lg:font-bold text-xl ml-4">{{ ticketsStore.ticketsCount.resolved }}</span>
             </div>
         </div>
         <div class="bg-white p-8 rounded-xl shadow-md">
             <div class="flex flex-col lg:flex-row justify-between">
-                <fwb-input v-model="searchFilter"label="Search" placeholder="enter your search query">
+                <fwb-input v-model="searchFilter" label="Search" placeholder="enter your search query">
                     <template #prefix>
                         <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none"
                             stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -122,10 +132,8 @@ ticketsStore.fetchTicketsOverview();
                 </fwb-input>
                 <div class="flex flex-col md:flex-row justify-between items-stretch md:items-end mt-4 lg:mt-0">
                     <div class="flex flex-col md:flex-row">
-                        <fwb-select class="mr-0 md:mr-4" v-model="statusFilter" :options="statuses"
-                            label="Status" />
-                        <fwb-select class="mt-4 md:mt-0" v-model="datesFilter" :options="dateOptions"
-                            label="Date" />
+                        <fwb-select class="mr-0 md:mr-4" v-model="statusFilter" :options="statuses" label="Status" />
+                        <fwb-select class="mt-4 md:mt-0" v-model="datesFilter" :options="dateOptions" label="Date" />
                     </div>
                     <fwb-button @click="openModal" v-if="!auth.user?.is_staff" class="md:ml-4 py-2.5 text-sm mt-4">New
                         Ticket</fwb-button>
@@ -137,7 +145,8 @@ ticketsStore.fetchTicketsOverview();
                     <Ticket :ticket="ticket" />
                 </li>
             </ul>
-            <span class="sm:text-2xl text-center block my-12" v-if="ticketsStore.tickets?.length == 0">No tickets to show...</span>
+            <span class="sm:text-2xl text-center block my-12" v-if="ticketsStore.tickets?.length == 0">No tickets to
+                show...</span>
         </div>
     </div>
     <fwb-modal v-if="isShowModal" @close="closeModal">
@@ -148,11 +157,10 @@ ticketsStore.fetchTicketsOverview();
         </template>
         <template #body>
             <form>
-                <fwb-input placeholder="Give a title to your ticket..." label="Title"
-                    v-model="newTicket.title" />
+                <fwb-input placeholder="Give a title to your ticket..." label="Title" v-model="newTicket.title" />
                 <span v-for="err in newTickerErrors.title" class="text-red-500 text-sm block mb-6">{{ err }}</span>
-                <fwb-textarea :validation-status="newTickerErrors.description?'error':''" label="Description" placeholder="Describe your new ticket..."
-                    v-model="newTicket.description" />
+                <fwb-textarea :validation-status="newTickerErrors.description ? 'error' : ''" label="Description"
+                    placeholder="Describe your new ticket..." v-model="newTicket.description" />
                 <span v-for="err in newTickerErrors.description" class="text-red-500 text-sm block">{{ err }}</span>
 
             </form>
